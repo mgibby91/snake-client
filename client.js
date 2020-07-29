541b2797d62936f6e179f8b0a64852bd88932318
Matthew Gibson <mattgibson6@gmail.com>
2020-07-29 19:24:44 +0000

    modified play.js to be able to handle future user inputs from key

--- client.js
+++ client.js
@@ -15,6 +15,9 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server.');
     conn.write('Name: MAG');
+    // setInterval(() => {
+    //   conn.write('Move: up');
+    // }, 1000)
   })
 
   return conn;
--- play.js
+++ play.js
@@ -4,5 +4,31 @@
 const { connect } = require('./client');
 
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+
+  if (key === 'b') {
+    process.stdout.write('hello');
+  }
+
+  if (key === '\u0003') {
+    process.stdout.write('Thanks for using me, ciao!');
+    process.exit();
+  }
+
+}
+
+
 console.log('Connecting...')
 connect();
+const mySetupInput = setupInput();
+
+mySetupInput.on('data', handleUserInput);
