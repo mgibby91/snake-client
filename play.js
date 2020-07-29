f2b06faaf5e4a7cf969fb7e4c780e27ea498c711
Matthew Gibson <mattgibson6@gmail.com>
2020-07-29 19:02:48 +0000

    started code modularization, created client.js

--- play.js
+++ play.js
@@ -1,20 +1,8 @@
 
-const net = require('net');
+// const net = require('net');
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50541
-  });
+const { connect } = require('./client');
 
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
 
 console.log('Connecting...')
 connect();
