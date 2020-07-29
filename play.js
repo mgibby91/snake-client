466d49b9ddbcd654fc6604f229460b0bf166722d
Matthew Gibson <mattgibson6@gmail.com>
2020-07-29 19:29:58 +0000

    created input.js to handle user inputs and exported function to play.js

--- play.js
+++ play.js
@@ -2,33 +2,9 @@
 // const net = require('net');
 
 const { connect } = require('./client');
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  return stdin;
-}
-
-const handleUserInput = function(key) {
-
-  if (key === 'b') {
-    process.stdout.write('hello');
-  }
-
-  if (key === '\u0003') {
-    process.stdout.write('Thanks for using me, ciao!');
-    process.exit();
-  }
-
-}
+const { setupInput } = require('./input')
 
 
 console.log('Connecting...')
 connect();
-const mySetupInput = setupInput();
-
-mySetupInput.on('data', handleUserInput);
+setupInput();;
